f5907ba5a3dfed40070ce9fe43f31a50b1ff78b0
Nicky <nicky@baidu.com>
2017-11-29 17:26:44 -0800

    Init check in for Android AICamera  with CMake and paddle lib

---
+++ Demo/Android/AICamera/app/paddle-android/third_party/openblas/lib/arm64-v8a/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.20.dev")
+SET(OpenBLAS_INCLUDE_DIRS /paddle/build_android/third_party/install/openblas/include)
+SET(OpenBLAS_LIBRARIES /paddle/build_android/third_party/install/openblas/lib/libopenblas.a)
