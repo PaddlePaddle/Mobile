72ccf0e5cdbb580130f2d50ae66cbe474b0f52e2
Nicky <nicky@baidu.com>
2017-12-12 22:13:31 -0800

    Remove paddle lib from git

--- Demo/Android/AICamera/app/paddle-android/third_party/openblas/lib/arm64-v8a/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.20.dev")
-SET(OpenBLAS_INCLUDE_DIRS /paddle/build_android/third_party/install/openblas/include)
-SET(OpenBLAS_LIBRARIES /paddle/build_android/third_party/install/openblas/lib/libopenblas.a)
