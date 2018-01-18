a972b43cd6e6c213aad646e632baa6c6798cdece
Nicky <nicky@baidu.com>
2018-01-18 15:27:43 -0800

    Create a activity and manager for settings. Fix multiple issues that cause image recognizer not working

--- Demo/Android/AICamera/app/src/main/java/com/paddlepaddle/aicamera/MainActivity.java
+++ Demo/Android/AICamera/app/src/main/java/com/paddlepaddle/aicamera/MainActivity.java
@@ -1,8 +1,10 @@
 package com.paddlepaddle.aicamera;
 
 import android.Manifest;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
@@ -21,20 +23,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Process;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Size;
-import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.CompoundButton;
-import android.widget.RadioButton;
-import android.widget.SeekBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Arrays;
@@ -42,10 +39,9 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SettingsManager.SettingsListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -68,21 +64,11 @@ public class MainActivity extends AppCompatActivity {
     private boolean mInProcessing;
     private boolean mCapturing;
 
-    private SSDModel mModel = SSDModel.PASCAL_MOBILENET_300;
-    private boolean mBackCamera = true;
-    private float mAccuracyThreshold = 0.3f; //only display above this threshold
-    private SharedPreferences mPrefs;
-    private boolean mSettingsShown;
-    private SettingsViewHolder mSettingsViewHolder;
-
     @BindView(R.id.textureView)
     AutoFitTextureView mTextureView;
 
     @BindView(R.id.ssdLayerView)
     SSDLayerView mSSDLayerView;
-
-    @BindView(R.id.settingsContainer)
-    ViewGroup mSettingsContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,8 +78,11 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        loadSettings();
-        mImageRecognizer = new ImageRecognizer(this, mModel);
+        SettingsManager.getInstance().loadSettings(this);
+        SettingsManager.getInstance().setListener(this);
+
+        mImageRecognizer = new ImageRecognizer(this, SettingsManager.getInstance().getModel());
+
     }
 
     private boolean hasPermission() {
@@ -130,8 +119,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
+
         startCaptureThread();
         startInferThread();
 
@@ -159,11 +149,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause() {
+    protected void onStop() {
         closeCamera();
         stopCaptureThread();
         stopInferThread();
-        super.onPause();
+        super.onStop();
     }
 
     private void startCaptureThread() {
@@ -216,7 +206,7 @@ public class MainActivity extends AppCompatActivity {
                 final CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
 
                 final Integer facing = characteristics.get(CameraCharacteristics.LENS_FACING);
-                if (facing != null && facing == (mBackCamera ? CameraCharacteristics.LENS_FACING_BACK : CameraCharacteristics.LENS_FACING_FRONT)) {
+                if (facing != null && facing == (SettingsManager.getInstance().isBackCamera() ? CameraCharacteristics.LENS_FACING_BACK : CameraCharacteristics.LENS_FACING_FRONT)) {
                     cameraIdAvailable = cameraId;
                     break;
                 }
@@ -279,7 +269,7 @@ public class MainActivity extends AppCompatActivity {
             captureRequestBuilder.addTarget(surface);
 
             mImageReader = ImageReader.newInstance(
-                    mPreviewSize.getWidth(), mPreviewSize.getHeight(), ImageFormat.YUV_420_888, 2);
+                    mPreviewSize.getWidth(), mPreviewSize.getHeight(), ImageFormat.YUV_420_888, 10);
 
             mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
                 @Override
@@ -293,42 +283,42 @@ public class MainActivity extends AppCompatActivity {
                         mRgbBytes = new byte[previewWidth * previewHeight * 3];
                     }
 
-                    final Image image = reader.acquireLatestImage();
+                    final Image image = reader.acquireNextImage();
                     if (image == null) return;
 
-                    if (mInProcessing) {
+                    if (mInProcessing || mInferHandler == null) {
                         image.close();
                         return;
                     }
 
                     mInProcessing = true;
 
+                    final byte[][] yuvBytes = new byte[3][];
+                    final Image.Plane[] planes = image.getPlanes();
+                    ImageUtils.fillBytes(planes, yuvBytes);
+                    final int yRowStride = planes[0].getRowStride();
+                    final int uvRowStride = planes[1].getRowStride();
+                    final int uvPixelStride = planes[1].getPixelStride();
 
                     mInferHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            final byte[][] yuvBytes = new byte[3][];
-                            final Image.Plane[] planes = image.getPlanes();
-                            ImageUtils.fillBytes(planes, yuvBytes);
-                            final int yRowStride = planes[0].getRowStride();
-                            final int uvRowStride = planes[1].getRowStride();
-                            final int uvPixelStride = planes[1].getPixelStride();
+                            if (mImageRecognizer != null) {
+                                ImageUtils.convertYUV420ToARGB8888(
+                                        yuvBytes[0],
+                                        yuvBytes[1],
+                                        yuvBytes[2],
+                                        previewWidth,
+                                        previewHeight,
+                                        yRowStride,
+                                        uvRowStride,
+                                        uvPixelStride,
+                                        mRgbBytes);
 
-                            ImageUtils.convertYUV420ToARGB8888(
-                                    yuvBytes[0],
-                                    yuvBytes[1],
-                                    yuvBytes[2],
-                                    previewWidth,
-                                    previewHeight,
-                                    yRowStride,
-                                    uvRowStride,
-                                    uvPixelStride,
-                                    mRgbBytes);
+                                List<SSDData> results = mImageRecognizer.infer(mRgbBytes, previewHeight, previewWidth, 3, SettingsManager.getInstance().getThreshold(), SettingsManager.getInstance().isBackCamera());
 
-                            List<SSDData> results = mImageRecognizer.infer(mRgbBytes, previewHeight, previewWidth, 3, mAccuracyThreshold, mBackCamera);
-
-                            mSSDLayerView.populateSSDList(results, mModel != SSDModel.FACE_MOBILENET_160);
-
+                                mSSDLayerView.populateSSDList(results, SettingsManager.getInstance().getModel() != SSDModel.FACE_MOBILENET_160);
+                            }
                             image.close();
                             mInProcessing = false;
                         }
@@ -408,144 +398,31 @@ public class MainActivity extends AppCompatActivity {
         mCapturing = false;
     }
 
-    //------- Settings ------------
-
-    class SettingsViewHolder {
-
-        @BindView(R.id.settingsView)
-        View mSettingsView;
-
-        @BindView(R.id.pascalRadioBtn)
-        RadioButton mPascalRadioBtn;
-
-        @BindView(R.id.faceRadioBtn)
-        RadioButton mFaceRadioBtn;
-
-        @BindView(R.id.backCameraRadioBtn)
-        RadioButton mBackCameraRadioBtn;
-
-        @BindView(R.id.frontCameraRadioBtn)
-        RadioButton mFrontCameraRadioBtn;
-
-        @BindView(R.id.accuracySlider)
-        SeekBar mAccuracySlider;
-
-        @BindView(R.id.accuracyText)
-        TextView mAccuracyText;
-
-        @OnCheckedChanged({R.id.backCameraRadioBtn, R.id.frontCameraRadioBtn})
-        public void onCameraSelected(CompoundButton button, boolean checked) {
-
-            if (checked) {
-                switch (button.getId()) {
-                    case R.id.backCameraRadioBtn:
-                        if (mBackCamera) return; //do not save or restart camera if no change
-                        mBackCamera = true;
-                        break;
-
-                    case R.id.frontCameraRadioBtn:
-                        if (!mBackCamera) return;
-                        mBackCamera = false;
-                        break;
-                }
-                SharedPreferences.Editor editor = mPrefs.edit();
-                editor.putBoolean(PREF_KEY_CAMERA, mBackCamera);
-                editor.commit();
-
-                closeCamera();
-                startCapture();
-                mImageRecognizer = new ImageRecognizer(MainActivity.this, mModel);
-            }
-        }
-
-        @OnCheckedChanged({R.id.pascalRadioBtn, R.id.faceRadioBtn})
-        public void onModelSelected(CompoundButton button, boolean checked) {
-            if (checked) {
-                switch (button.getId()) {
-                    case R.id.pascalRadioBtn:
-                        if (mModel == SSDModel.PASCAL_MOBILENET_300) return;
-                        mModel = SSDModel.PASCAL_MOBILENET_300;
-                        break;
-
-                    case R.id.faceRadioBtn:
-                        if (mModel == SSDModel.FACE_MOBILENET_160) return;
-                        mModel = SSDModel.FACE_MOBILENET_160;
-                        break;
-                }
-                SharedPreferences.Editor editor = mPrefs.edit();
-                editor.putString(PREF_KEY_MODEL, mModel.modelFileName);
-                editor.commit();
-
-                mImageRecognizer = new ImageRecognizer(MainActivity.this, mModel);
-            }
-        }
-    }
-
-    private static final String PREF_NAME = "prefs";
-    private static final String PREF_KEY_THRESHOLD = "accuracy_threshold";
-    private static final String PREF_KEY_MODEL = "ssd_model";
-    private static final String PREF_KEY_CAMERA = "camera";
-
-
     @OnClick(R.id.ssdLayerView)
     public void onLayerClick(View v) {
-        if (!mSettingsShown) {
-            LayoutInflater inflater = getLayoutInflater();
-            View settingsView = inflater.inflate(R.layout.settings, mSettingsContainer, true);
-            mSettingsViewHolder = new SettingsViewHolder();
-            ButterKnife.bind(mSettingsViewHolder, settingsView);
-            populateSettingsView();
-            mSettingsShown = true;
-        } else {
-            mSettingsContainer.removeAllViews();
-            mSettingsShown = false;
-        }
-
+        startActivity(new Intent(this, SettingsActivity.class));
+        overridePendingTransition(0, 0);
     }
 
-    private void loadSettings() {
-        mPrefs = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
-        mBackCamera = mPrefs.getBoolean(PREF_KEY_CAMERA, true);
-        mAccuracyThreshold = mPrefs.getFloat(PREF_KEY_THRESHOLD, 0.3f);
-        String modelFileName = mPrefs.getString(PREF_KEY_MODEL, SSDModel.PASCAL_MOBILENET_300.modelFileName);
-        mModel = SSDModel.fromModelFileName(modelFileName);
+    @Override
+    public void onCameraChanged(boolean backCamera) {
+        closeCamera();
+        startCapture();
     }
 
-    private void populateSettingsView() {
-        switch (mModel) {
-            case PASCAL_MOBILENET_300:
-                mSettingsViewHolder.mPascalRadioBtn.setChecked(true);
-                break;
-            case FACE_MOBILENET_160:
-                mSettingsViewHolder.mFaceRadioBtn.setChecked(true);
-                break;
-        }
+    @Override
+    public void onModelChanged(SSDModel ssdModel) {
+        //restart app to load new model, creating ImageRecognizer seems to have issues
+        AlarmManager alm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alm.set(AlarmManager.RTC, System.currentTimeMillis() + 1000, PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()), 0));
+        Process.killProcess(Process.myPid());
+    }
 
-        if (mBackCamera) {
-            mSettingsViewHolder.mBackCameraRadioBtn.setChecked(true);
-        } else {
-            mSettingsViewHolder.mFrontCameraRadioBtn.setChecked(true);
-        }
+    @Override
+    public void onThresholdChanged(float threshold) {
+    }
 
-        mSettingsViewHolder.mAccuracySlider.setProgress((int)(mAccuracyThreshold * 100));
-        mSettingsViewHolder.mAccuracyText.setText(mAccuracyThreshold + "");
-
-        mSettingsViewHolder.mAccuracySlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mAccuracyThreshold = progress / 10 / 10.f;
-                mSettingsViewHolder.mAccuracyText.setText(mAccuracyThreshold + "");
-
-                SharedPreferences.Editor editor = mPrefs.edit();
-                editor.putFloat(PREF_KEY_THRESHOLD, mAccuracyThreshold);
-                editor.commit();
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-            }
-        });
+    @Override
+    public void onSettingsClose() {
     }
 }
